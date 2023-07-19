584c598e7069ed1f26caa6bd8aeb997198780521
Tensorboy2 <svargdal@online.no>
2023-07-19 13:23:05 +0200

    ee

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ const ctx = canvas.getContext('2d');
 const g = 9.81; // gravitational constant
 const arm1Length = 100;
 const arm2Length = 100;
-const mass1 = 1;
-const mass2 = 2;
+const mass1 = 10;
+const mass2 = 10;
 
 // Variables to track angles and angular velocities
-let angle1 = Math.PI / 2;
-let angle2 = Math.PI / 2;
+let angle1 = randomAngle();
+let angle2 = randomAngle();
 let angle1Velocity = 0;
 let angle2Velocity = 0;
+
+// Function to generate a random initial angle between 0 and 2 * PI
+function randomAngle() {
+  return Math.random() * (2 * Math.PI);
+}
 
 // Function to update the double pendulum positions
 function updatePendulum() {
@@ -42,20 +47,21 @@ function drawPendulum() {
   const x2 = x1 + arm2Length * Math.sin(angle2);
   const y2 = y1 + arm2Length * Math.cos(angle2);
 
+  ctx.strokeStyle = 'blue';
   ctx.beginPath();
   ctx.moveTo(canvas.width / 2, canvas.height / 3);
   ctx.lineTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.stroke();
 
+  ctx.fillStyle = 'red';
   ctx.beginPath();
   ctx.arc(x1, y1, mass1, 0, 2 * Math.PI);
-  ctx.fillStyle = 'blue';
   ctx.fill();
 
+  ctx.fillStyle = 'green';
   ctx.beginPath();
   ctx.arc(x2, y2, mass2, 0, 2 * Math.PI);
-  ctx.fillStyle = 'blue';
   ctx.fill();
 }
 

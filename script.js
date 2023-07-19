556fc63d11f514d650268da49cb61f4f12dd7747
Tensorboy2 <svargdal@online.no>
2023-07-19 13:37:30 +0200

    b

--- script.js
+++ script.js
@@ -6,63 +6,78 @@ const arm1Length = 100;
 const arm2Length = 100;
 const mass1 = 10;
 const mass2 = 10;
-
-// Variables to track angles and angular velocities
-let angle1 = randomAngle();
-let angle2 = randomAngle();
-let angle1Velocity = 0;
-let angle2Velocity = 0;
+const numPendulums = 100;
 
 // Function to generate a random initial angle between 0 and 2 * PI
 function randomAngle() {
   return Math.random() * (2 * Math.PI);
 }
 
-// Function to update the double pendulum positions
-function updatePendulum() {
-  const num1 = -g * (2 * mass1 + mass2) * Math.sin(angle1);
-  const num2 = -mass2 * g * Math.sin(angle1 - 2 * angle2);
-  const num3 = -2 * Math.sin(angle1 - angle2) * mass2;
-  const num4 = angle2Velocity * angle2Velocity * arm2Length + angle1Velocity * angle1Velocity * arm1Length * Math.cos(angle1 - angle2);
-  const den = arm1Length * (2 * mass1 + mass2 - mass2 * Math.cos(2 * angle1 - 2 * angle2));
-  const angle1Acceleration = (num1 + num2 + num3 * num4) / den;
-
-  const num5 = 2 * Math.sin(angle1 - angle2) * (angle1Velocity * angle1Velocity * arm1Length * (mass1 + mass2));
-  const num6 = g * (mass1 + mass2) * Math.cos(angle1);
-  const num7 = angle2Velocity * angle2Velocity * arm2Length * mass2 * Math.cos(angle1 - angle2);
-  const den2 = arm2Length * (2 * mass1 + mass2 - mass2 * Math.cos(2 * angle1 - 2 * angle2));
-  const angle2Acceleration = (num5 + num6 + num7) / den2;
-
-  angle1Velocity += angle1Acceleration;
-  angle2Velocity += angle2Acceleration;
-  angle1 += angle1Velocity;
-  angle2 += angle2Velocity;
+// Function to create an array of random initial conditions for the pendulums
+function generateRandomInitialConditions() {
+  const initialConditions = [];
+  for (let i = 0; i < numPendulums; i++) {
+    initialConditions.push({
+      angle1: randomAngle(),
+      angle2: randomAngle(),
+      angle1Velocity: 0,
+      angle2Velocity: 0,
+      tracerColor: `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 0.8)`
+    });
+  }
+  return initialConditions;
 }
 
-// Function to draw the double pendulum on the canvas
+// Variables to track angles and angular velocities for each pendulum
+let pendulums = generateRandomInitialConditions();
+
+// Function to update the double pendulum positions for each pendulum
+function updatePendulum() {
+  for (let i = 0; i < numPendulums; i++) {
+    const pendulum = pendulums[i];
+
+    const num1 = -g * (2 * mass1 + mass2) * Math.sin(pendulum.angle1);
+    const num2 = -mass2 * g * Math.sin(pendulum.angle1 - 2 * pendulum.angle2);
+    const num3 = -2 * Math.sin(pendulum.angle1 - pendulum.angle2) * mass2;
+    const num4 = pendulum.angle2Velocity * pendulum.angle2Velocity * arm2Length + pendulum.angle1Velocity * pendulum.angle1Velocity * arm1Length * Math.cos(pendulum.angle1 - pendulum.angle2);
+    const den = arm1Length * (2 * mass1 + mass2 - mass2 * Math.cos(2 * pendulum.angle1 - 2 * pendulum.angle2));
+    const angle1Acceleration = (num1 + num2 + num3 * num4) / den;
+
+    const num5 = 2 * Math.sin(pendulum.angle1 - pendulum.angle2) * (pendulum.angle1Velocity * pendulum.angle1Velocity * arm1Length * (mass1 + mass2));
+    const num6 = g * (mass1 + mass2) * Math.cos(pendulum.angle1);
+    const num7 = pendulum.angle2Velocity * pendulum.angle2Velocity * arm2Length * mass2 * Math.cos(pendulum.angle1 - pendulum.angle2);
+    const den2 = arm2Length * (2 * mass1 + mass2 - mass2 * Math.cos(2 * pendulum.angle1 - 2 * pendulum.angle2));
+    const angle2Acceleration = (num5 + num6 + num7) / den2;
+
+    pendulum.angle1Velocity += angle1Acceleration;
+    pendulum.angle2Velocity += angle2Acceleration;
+    pendulum.angle1 += pendulum.angle1Velocity * 0.01; // Adjust the time step to control the speed
+    pendulum.angle2 += pendulum.angle2Velocity * 0.01; // Adjust the time step to control the speed
+  }
+}
+
+// Function to draw the double pendulum on the canvas for each pendulum
 function drawPendulum() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  const x1 = canvas.width / 2 + arm1Length * Math.sin(angle1);
-  const y1 = canvas.height / 3 + arm1Length * Math.cos(angle1);
-  const x2 = x1 + arm2Length * Math.sin(angle2);
-  const y2 = y1 + arm2Length * Math.cos(angle2);
 
-  ctx.strokeStyle = 'blue';
-  ctx.beginPath();
-  ctx.moveTo(canvas.width / 2, canvas.height / 3);
-  ctx.lineTo(x1, y1);
-  ctx.lineTo(x2, y2);
-  ctx.stroke();
+  for (let i = 0; i < numPendulums; i++) {
+    const pendulum = pendulums[i];
+    const x1 = canvas.width / 2 + arm1Length * Math.sin(pendulum.angle1);
+    const y1 = canvas.height / 3 + arm1Length * Math.cos(pendulum.angle1);
+    const x2 = x1 + arm2Length * Math.sin(pendulum.angle2);
+    const y2 = y1 + arm2Length * Math.cos(pendulum.angle2);
 
-  ctx.fillStyle = 'red';
-  ctx.beginPath();
-  ctx.arc(x1, y1, mass1, 0, 2 * Math.PI);
-  ctx.fill();
+    ctx.strokeStyle = pendulum.tracerColor;
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, canvas.height / 3);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
 
-  ctx.fillStyle = 'green';
-  ctx.beginPath();
-  ctx.arc(x2, y2, mass2, 0, 2 * Math.PI);
-  ctx.fill();
+    ctx.fillStyle = pendulum.tracerColor;
+    ctx.beginPath();
+    ctx.arc(x2, y2, 1.5, 0, 2 * Math.PI);
+    ctx.fill();
+  }
 }
 
 // Animation loop
